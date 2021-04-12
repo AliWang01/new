085896566aa5961ac171706eeb3bbd63c16a77f9
AliWang01 <78440745+AliWang01@users.noreply.github.com>
2021-04-11 21:11:22 -0400

    Add files via upload

---
+++ test_hello.py
@@ -0,0 +1,18 @@
+from math import factorial
+
+import hello
+
+
+# def test_says_world():
+# assert hello.say_what() == 'world'
+
+def test_fibonacci_1(self):
+    self.assertEqual(hello.fibonacci(1), 1)
+
+
+def test_fibonacci_1(self):
+    self.assertEqual(hello.fibonacci(10), 89)
+
+
+def test_factorial_1(self):
+    self.assertEqual(factorial(1), 1)
